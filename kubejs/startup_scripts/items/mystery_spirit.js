644fb9ee4c5382562ff1c7bea5785dd495d47d04
hyleseum <hyleseum@gmail.com>
2026-04-23 16:47:39 +0200

    FirstCommit

--- kubejs/startup_scripts/items/mystery_spirit.js
+++
@@ -1,7 +0,0 @@
-StartupEvents.registry('item', event => {
-    event.create('mystery_spirit')
-        .displayName('§5Mystery Spirit')
-        .tooltip('§7The Void has yet to reveal its nature...')
-        .rarity('rare')
-        .glow(true)
-})
