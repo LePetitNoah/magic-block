bee2b7d10a585d8da6e40b7b400fc8855c4b2308
hyleseum <hyleseum@gmail.com>
2026-04-29 18:40:38 +0200

    Modification de l'apparence du spirit extractor #10

--- kubejs/startup_scripts/blocks/spirit_extractor.js
+++ kubejs/startup_scripts/blocks/spirit_extractor.js
@@ -1,4 +1,10 @@
 StartupEvents.registry('block', event => {
-    event.create('spirit_extractor')
-        .displayName('§5Spirit Extractor')
+    event.create('spirit_extractor_idle')
+        .displayName('§5Spirit Extractor (Idle)')
+
+    event.create('spirit_extractor_running')
+        .displayName('§5Spirit Extractor (Running)')
+
+    event.create('spirit_extractor_errored')
+        .displayName('§5Spirit Extractor (Errored)')
 })
